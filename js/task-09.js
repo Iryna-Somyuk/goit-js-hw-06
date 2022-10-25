c0a2509a26bde4b415025013cd0825f8ee37cb98
Iryna-Somyuk <Iryna.Somuyk@gmail.com>
2022-10-25 17:13:02 +0300

    завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+}
+
+const ChangeColor = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', onSeach);
+function onSeach () {
+  document.body.style.backgroundColor = getRandomHexColor();
+
+
 }
